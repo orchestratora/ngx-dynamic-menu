6f44a21f919e50f2cb170f538c052211026a8e8b
Alex Malkevich <alex.malkevich@mcmakler.de>
2019-02-11 22:09:20 +0100

    test(service): make unit tests to pass
    As they are empty now

--- projects/dynamic-menu/src/lib/dynamic-menu.service.spec.ts
+++ projects/dynamic-menu/src/lib/dynamic-menu.service.spec.ts
@@ -1,10 +1,20 @@
-import { TestBed, async, inject } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { DynamicMenuExtrasToken } from './dynamic-menu-extras';
 import { DynamicMenuService } from './dynamic-menu.service';
 
 describe('Service: DynamicMenu', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [DynamicMenuService],
+      imports: [RouterTestingModule.withRoutes([])],
+      providers: [
+        DynamicMenuService,
+        {
+          provide: DynamicMenuExtrasToken,
+          useValue: new DynamicMenuExtrasToken(),
+        },
+      ],
     });
   });
 
