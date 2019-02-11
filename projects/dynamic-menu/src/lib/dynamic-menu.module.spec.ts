f106ed0985e13450bc32b089bbdab41ec59eb1d4
Alex Malkevich <alex.malkevich@mcmakler.de>
2019-02-11 20:31:51 +0100

    test(module): add main module test
    So tests do not fail

---
+++ projects/dynamic-menu/src/lib/dynamic-menu.module.spec.ts
@@ -0,0 +1,7 @@
+import { DynamicMenuModule } from './dynamic-menu.module';
+
+describe('Module: DynamicMenu', () => {
+  it('should exist', () => {
+    expect(DynamicMenuModule).toBeTruthy();
+  });
+});
